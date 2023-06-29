6f1d69834d53002ccb1f1796e4f5bee97ec7e658
samery46 <syamme@ymail.com>
2023-06-29 17:19:06 +0700

    Initilaize Project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+@section('pageTitle', 'Home')
+@section('main')
+  <div class="container">
+    <div class="main">
+      <div class="task-summary-container">
+       <h1 class="task-summary-heading">Summary of Your Tasks</h1>
+
+      <div  class="task-summary-list">
+        <span class="material-icons">check_circle</span>
+        <h2>You have completed 1 task</h2>
+      </div>
+
+      <div class="task-summary-list">
+        <span class="material-icons">list</span>
+        <h2>You still have 5 tasks left</h2>
+      </div>
+    </div>
+  </div>
+@endsection
